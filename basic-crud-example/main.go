20ae43fb26de27f636fa67cef41c222b2932ed11
elameendaiyabu <elameendaiyabu@gmail.com>
2024-08-07 23:56:56 +0000

    getMovies endpoint done

---
+++ basic-crud-example/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	Id       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	FirstName string `json:"firstName"`
+	LastName  string `json:"lastName"`
+}
+
+var movies []Movie
+
+func main() {
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{Id: "1", Isbn: "3245", Title: "movie-1", Director: &Director{FirstName: "john", LastName: "doe"}})
+	movies = append(movies, Movie{Id: "2", Isbn: "8293", Title: "movie-2", Director: &Director{FirstName: "micheal", LastName: "forbes"}})
+
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("/movies/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("starting server at port: 8080 \n")
+
+	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
